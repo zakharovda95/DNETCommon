6cc6252d8a2a8884554ff8cf70716703bb3d10bc
dmitryzakharov <zakharovda95@gmail.com>
2024-11-16 17:28:44 +0300

    add static members into TimeNowProvider.cs

--- DNETCommon/Utils/TimeNowProvider.cs
+++ DNETCommon/Utils/TimeNowProvider.cs
@@ -8,6 +8,9 @@ public class TimeNowProvider : ITimeNowProvider
     public DateTime Now => DateTime.Now;
     public DateTime NowUtc => DateTime.UtcNow;
     
+    public static DateTime NowStatic => DateTime.Now;
+    public static DateTime NowUtcStatic => DateTime.UtcNow;
+    
     public long ToUnix(DateTimeFormatEnum format = DateTimeFormatEnum.Local)
     {
         return format switch
@@ -24,6 +27,26 @@ public class TimeNowProvider : ITimeNowProvider
         {
             DateTimeFormatEnum.Local => Now.ToString("o"),
             DateTimeFormatEnum.Utc => NowUtc.ToString("o"),
+            _ => string.Empty
+        };
+    }
+    
+    public static long ToUnixStatic(DateTimeFormatEnum format = DateTimeFormatEnum.Local)
+    {
+        return format switch
+        {
+            DateTimeFormatEnum.Local => new DateTimeOffset(NowStatic).ToUnixTimeMilliseconds(),
+            DateTimeFormatEnum.Utc => new DateTimeOffset(NowUtcStatic).ToUnixTimeMilliseconds(),
+            _ => -1
+        };
+    }
+
+    public static string ToIsoStatic(DateTimeFormatEnum format = DateTimeFormatEnum.Local)
+    {
+        return format switch
+        {
+            DateTimeFormatEnum.Local => NowStatic.ToString("o"),
+            DateTimeFormatEnum.Utc => NowUtcStatic.ToString("o"),
             _ => string.Empty
         };
     }
